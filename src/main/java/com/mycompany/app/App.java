bf7d4ba873b93fbb3eb98fd9ab57ce735c7ceba7
Aslah EA 🔧 | DevOps @OpsForge <152137279+aslahea@users.noreply.github.com>
2025-10-24 15:41:07 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,13 +1,18 @@
 package com.mycompany.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+    
     public static void main( String[] args )
     {
-        System.out.println( " HI ... Hello World!" );
+        logger.info( "Hi ... Hello World!" );
     }
 }
